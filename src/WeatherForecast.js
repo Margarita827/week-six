4c41675de3115043b58fedb78fce5a8d902caed6
Margarita827 <margaiba5@gmail.com>
2024-04-23 22:32:55 +0100

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,57 +2,19 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 
-
-
 export default function WeatherForecast() {
-    return (
-      <div className="WeatherForecast">
-        <div className="row">
-          <div className="col">
-            <div className="WeatherForecast-day">Thursday</div>
-            <WeatherIcon code="clear-sky-day" />
-            <div className="WeatherForecast-temperatures">
-              <span className="temperature-max">19º</span>
-              <span className="temperature-min">10º</span>
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="WeatherForecast-day">Thursday</div>
-            <WeatherIcon code="clear-sky-day" />
-            <div className="WeatherForecast-temperatures">
-              <span className="temperature-max">19º</span>
-              <span className="temperature-min">10º</span>
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="WeatherForecast-day">Thursday</div>
-            <WeatherIcon code="clear-sky-day" />
-            <div className="WeatherForecast-temperatures">
-              <span className="temperature-max">19º</span>
-              <span className="temperature-min">10º</span>
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="WeatherForecast-day">Thursday</div>
-            <WeatherIcon code="clear-sky-day" />
-            <div className="WeatherForecast-temperatures">
-              <span className="temperature-max">19º</span>
-              <span className="temperature-min">10º</span>
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="WeatherForecast-day">Thursday</div>
-            <WeatherIcon code="clear-sky-day" />
-            <div className="WeatherForecast-temperatures">
-              <span className="temperature-max">19º</span>
-              <span className="temperature-min">10º</span>
-            </div>
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thursday</div>
+          <WeatherIcon code="clear-sky-day" />
+          <div className="WeatherForecast-temperatures">
+            <span className="temperature-max">19º</span>
+            <span className="temperature-min">10º</span>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
