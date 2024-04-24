b5c4dff1c3efb40f1aa7430ffc6f0827975c17d2
Margarita827 <margaiba5@gmail.com>
2024-04-24 06:43:31 +0100

    noopener

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
           <a
             href="https://github.com/Margarita827/week-five"
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
           </a>
